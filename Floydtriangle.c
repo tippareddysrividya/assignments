38860b70c3158895e21290f632af7e67c8e4b206
tippareddysrividya <vidya.reddy003@gmail.com>
2016-01-07 14:29:03 +0530

    Update Floydtriangle.c

--- Floydtriangle.c
+++ Floydtriangle.c
@@ -1,1 +1,18 @@
 #include<stdio.h>
+void  main()
+{
+	int n,i,j,k=1;
+	printf("enter the no. of lines of triangle:\n");
+	scanf("%d",&n);
+	printf("the triangle is\n");
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+			printf("%d\t",k);
+			k++;
+		}
+		printf("\n");
+	}
+
+}
