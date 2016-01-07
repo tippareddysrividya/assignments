c89638485e891fe302737032f6bf9d1fd5ce6a85
tippareddysrividya <vidya.reddy003@gmail.com>
2016-01-07 15:23:43 +0530

    Create pascalTriangle.c

---
+++ pascalTriangle.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+    int x,y,n,a,z,s;
+    printf("Enter the limit: ");
+    scanf("%d",&n);
+    printf("\n\n");
+    s=n;
+    for(x=0; x<=n; x++)
+    {
+        a=1;
+        s--;
+        for(y=0; y<=x; y++)
+        {
+            printf("%d ",a);
+            a=(a*(x-y)/(y+1));
+        }
+        printf("\n");
+    }
+}
